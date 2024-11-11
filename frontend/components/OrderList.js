27d26b45601b587a31d5ae1c8f9fcdabd8d7352c
wprince0288 <wprince0288@gmail.com>
2024-11-11 17:28:15 -0600

    store started

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,34 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchOrderHistory, setSizeFilter } from '../state/store'
 
 export default function OrderList() {
-  const orders = []
+  const dispatch = useDispatch();
+  const { orders, loading, error } = useSelector((state) => state.orderHistory)
+  const sizeFilter = useSelector((state) => state.sizeFilter)
+
+  useEffect(() => {
+    dispatch(fetchOrderHistory());
+  }, [dispatch])
+
+  const handleFilterClick = (size) => {
+    dispatch(setSizeFilter(size))
+  }
+
+  const filteredOrders = sizeFilter === 'ALL' ? orders : orders.filter(order => order.size === sizeFilter)
+
   return (
     <div id="orderList">
       <h2>Pizza Orders</h2>
+      {loading && <p>Loading orders...</p>}
+      {error && <p>Error: {error}</p>}
       <ol>
-        {
-          orders.map(() => {
-            return (
-              <li key={1}>
-                <div>
-                  order details here
-                </div>
-              </li>
-            )
-          })
-        }
+        {filteredOrders.map((order) => (
+          <li key={order.id}>
+            <div>
+              <p><strong>Name:</strong> {order.fullName}</p>
+              <p><strong>Size:</strong> {order.size}</p>
+              <p><strong>Toppings:</strong> {order.toppings.join(' ')}</p>
+            </div>
+          </li>
+        ))}
       </ol>
       <div id="sizeFilters">
         Filter by size:
-        {
-          ['All', 'S', 'M', 'L'].map(size => {
-            const className = `button-filter${size === 'All' ? ' active' : ''}`
-            return <button
-              data-testid={`filterBtn${size}`}
-              className={className}
-              key={size}>{size}</button>
-          })
-        }
+        {['All', 'S', 'M', 'L'].map(size => (
+          <button
+            data-testid={`filterBtn${size}`}
+            className={`button-filter${size === sizeFilter ? ' active' : ''}`}
+            key={size}
+            onClick={() => handleFilterClick(size)}
+          >
+            {size}
+          </button>
+        ))}
       </div>
     </div>
   )
